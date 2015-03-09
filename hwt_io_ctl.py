81351c1e3f595fa01b44c523a10ac13cd08ea53b
Lukasz Majewski <l.majewski@samsung.com>
2015-03-09 15:48:09 +0100

    HWT: PWT: IO: Support for proper HWT device recognition
    Signed-off-by: Lukasz Majewski <l.majewski@samsung.com>

--- hwt_io_ctl.py
+++ hwt_io_ctl.py
@@ -5,6 +5,8 @@ Control GPIO connected via a FTDI FT232R module using pylibftdi
 Copyright (c) 2014 Lukasz Majewski <l.majewski@samsung.com>
 License: GPLv2+
 """
+import usb
+import sys
 import argparse
 from pylibftdi import BitBangDevice
 
@@ -59,3 +61,24 @@ if __name__ == '__main__':
 	if args.debug_en:
 		print "HWT PWR control (FTDI based) - %s" % \
 		    str(datetime.datetime.now())[:19]
+
+	# idVendor=0x0403, idProduct=0x6001
+	# are for FTDI USB <-> Serial converter
+	# they shouldn't change -> conpare iSerialNumber
+
+	dev = usb.core.find(idVendor=0x0403, idProduct=0x6001)
+	if dev is None:
+		print 'HWT PWR device is not connected!'
+		sys.exit()
+	else:
+		iSerialNumber = usb.util.get_string(dev, dev.iSerialNumber)
+		if iSerialNumber != args.device:
+			print "iSerialNumber: %s does not match %s" % \
+			       (iSerialNumber, args.device)
+			sys.exit()
+
+
+		# if dev._serial is None:
+		# 	print "AAAA"
+		# 	dev._serial = usb.util.get_string(dev, dev.iSerial)
+		# print "Serial -> %s" % dev.serial
